f4d3430d2dece36fb010120e9501a75a5232bb49
blaserlab <bradley.blaser@osumc.edu>
2023-06-23 16:20:52 -0400

    upgraded bt

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,30 +1,3 @@
-# renv --------------------------------------------------------------------
-
-# set up the renv from scratch
-
-# renv::init(bioconductor = TRUE)
-
-# restore the renv from the lockfile
-
-# renv::restore()
-
-
-
-# package installation ----------------------------------------------------
-
-# # Try this first...it's faster:
-# blaseRtemplates::easy_install("patchwork", how = "link_from_cache")
-
-# # If you need a new package or an update, try this:
-# blaseRtemplates::easy_install("", how = "new_or_update")
-# blaseRtemplates::easy_install("ggnewscale", how = "new_or_update")
-
-# # If you are installing from a "tarball", use this:
-# blaseRtemplates::easy_install("/path/to/tarball.tar.gz")
-
-# # use "bioc::<package name>" for bioconductor packages
-# # use "<repo/package name>" for github source packages
-
 # load and attach packages ------------------------------------------------
 suppressPackageStartupMessages(library("conflicted"))
 suppressPackageStartupMessages(library("blaseRtools"))
@@ -43,10 +16,7 @@ suppressPackageStartupMessages(library("stats"))
 suppressPackageStartupMessages(library("ggtext"))
 suppressPackageStartupMessages(library("CellChat"))
 
-# uncomment and use the following to install or update the data package---------------------------------------
-bb_renv_datapkg(path = "/home/OSUMC.EDU/blas02/network/X/Labs/Blaser/share/collaborators/bakerlab_flt3_gilt_project/datapkg")
 
 # use this to load the data package-------------------------------------
+blaseRtemplates::project_data(path = "/home/OSUMC.EDU/blas02/network/X/Labs/Blaser/share/collaborators/bakerlab_flt3_gilt_project/datapkg")
 
-# run once to load, run again to unload
-requireData(package = "flt3.aml.bakerlab.datapkg")
